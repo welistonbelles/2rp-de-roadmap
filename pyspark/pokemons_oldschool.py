273a8cc4a8c07f5d0f530287ada1dad2423e57ba
Weliston Belles <weliston2012@gmail.com>
2022-03-29 18:43:10 -0300

    Finalizado modulo PySpark

---
+++ pyspark/pokemons_oldschool.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.getOrCreate()
+
+## Realizando o load dos dados presentes nas tabelas e instanciando os dataframes
+df_poke = spark.table('work_dataeng.pokemon_weliston')
+df_gen = spark.table('work_dataeng.generation_weliston')
+
+## Aplicando o filtro de data
+df_gen = df_gen.filter(df_gen["date_introduced"] < '2002-11-21')
+
+## Realizando o cache do data frame
+df_gen.cache()
+
+## Realizando o inner join
+df_generation_join = df_gen.join(df_poke, 'generation', how='inner')
+
+## Salvando o dataframe em uma tabela
+df_generation_join.createOrReplaceTempView("tmpTableView") 
+spark.sql("CREATE TABLE IF NOT EXISTS work_dataeng.pokemons_oldschool_weliston as select * from tmpTableView");
